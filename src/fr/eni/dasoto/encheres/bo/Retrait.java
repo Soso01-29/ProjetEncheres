f4980ef9a47badfce86b8ab8510a58a8f1f11255
Solène <solene.grelaud@gmail.com>
2021-01-12 16:11:36 +0100

    Ajout de la couche BO

---
+++ src/fr/eni/dasoto/encheres/bo/Retrait.java
@@ -0,0 +1,59 @@
+package fr.eni.dasoto.encheres.bo;
+
+public class Retrait {
+
+	private Integer noArticle;
+	private String rue;
+	private String codePostal;
+	private String ville;
+
+	public Retrait() {
+
+	}
+
+	public Retrait(String rue, String codePostal, String ville) {
+
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	public Integer getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(Integer noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	@Override
+	public String toString() {
+		return "Retrait [noArticle=" + noArticle + ", rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville
+				+ "]";
+	}
+
+}
