430105f63328bb5805e032385e398616ca535671
Solène <solene.grelaud@gmail.com>
2021-01-12 16:11:36 +0100

    Ajout de la couche BO

---
+++ src/fr/eni/dasoto/encheres/bo/ArticleVendu.java
@@ -0,0 +1,124 @@
+package fr.eni.dasoto.encheres.bo;
+
+import java.time.LocalDate;
+
+public class ArticleVendu {
+
+	private Integer noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private Integer miseAPrix;
+	private Integer prixVente;
+	private String etatVente;
+	private Integer noUtilisateur;
+	private Integer categorieArticle;
+
+	public ArticleVendu() {
+
+	}
+
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEncheres, LocalDate dateFinEncheres,
+			Integer miseAPrix, Integer prixVente, String etatVente, Integer noUtilisateur, Integer categorieArticle) {
+
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+		this.noUtilisateur = noUtilisateur;
+		this.categorieArticle = categorieArticle;
+	}
+
+	public Integer getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(Integer noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+
+	public Integer getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(Integer miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public Integer getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(Integer prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public String getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(String etatVente) {
+		this.etatVente = etatVente;
+	}
+
+	public Integer getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(Integer noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public Integer getCategorieArticle() {
+		return categorieArticle;
+	}
+
+	public void setCategorieArticle(Integer categorieArticle) {
+		this.categorieArticle = categorieArticle;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente + ", noUtilisateur="
+				+ noUtilisateur + ", categorieArticle=" + categorieArticle + "]";
+	}
+
+}
